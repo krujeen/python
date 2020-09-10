42ed1dfa4717d67ad85e88660bccf1cadf0cb237
KruJeen <44271005+krujeen@users.noreply.github.com>
2020-09-10 13:18:14 +0700

    Create if_grade1.py

---
+++ if_grade1.py
@@ -0,0 +1,13 @@
+#กรณีที่ต้องการเอาไว้เช็คหลายๆเงื่อนไข โดยใช้คีเวิร์ด elif เช่น
+
+point = 82
+if point >= 80:
+  print("Grade A")
+elif point < 80 and point >= 70:
+	print("Grade B")
+elif point < 70 and point >= 60:
+	print("Grade C")
+elif point < 60 and point >= 50:
+ 	print("Grade D")
+else:
+	print("FFFFFFF")
