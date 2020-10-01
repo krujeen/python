0d5b835d10db79930790d630296850d4857b0324
KruJeen <44271005+krujeen@users.noreply.github.com>
2020-10-01 13:59:06 +0700

    Update and rename loop_for_print0-4.py to loop_for_print.py

---
+++ loop_for_print.py
@@ -0,0 +1,9 @@
+#For Loop มีรูปแบบ for i in range: เช่น วนลูปปริ้น 0-10
+for i in range(11):
+  print(i)
+
+  
+    
+#For Loop มีรูปแบบ for i in range: เช่น วนลูปปริ้น 1-10
+for i in range(1,10):
+	print(i)
--- loop_for_print0-4.py
+++
@@ -1,4 +0,0 @@
-#For Loop มีรูปแบบ for i in range: เช่น วนลูปปริ้น 0-4
-
-for i in range(5):
-  print(i)
