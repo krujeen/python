2d21cb1b6ab385ab5f46d9a70c4cf8c9543d948b
KruJeen <44271005+krujeen@users.noreply.github.com>
2020-10-01 16:11:35 +0700

    Update print2digi.py

--- print2digi.py
+++ print2digi.py
@@ -2,6 +2,9 @@ a,b,c = [int(e) for e in input().split()]
 
 result = a*b/c
 
+
+# แสดง ทศนิยม 2 ตำแหน่ง ด้วยคำสั่ง round
 print (round(result,2),"cm.")
 
+# แสดง ทศนิยม 2 ตำแหน่ง ด้วย %.2f
 print ('%.2f cm.'%(result))
