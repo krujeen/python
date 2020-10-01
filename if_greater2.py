fd6702bc692a130355b5ba8537c503485fb8732b
KruJeen <44271005+krujeen@users.noreply.github.com>
2020-10-01 13:44:18 +0700

    Create if_greater2.py

---
+++ if_greater2.py
@@ -0,0 +1,11 @@
+# รับค่า a และ b
+a = int(intput())
+b = int(intput())
+
+# เปรียบเทียบว่า a มากกว่า เท่ากับ หรือน้อยกว่า b
+if a > b:
+  print("a มากกว่า b")
+elif a == b:
+  print("a เท่ากับ b")
+else:
+  print("a น้อยกว่า b")
