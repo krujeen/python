9ae1a4075b91bf8979d2366d4b8aad459af84ad9
KruJeen <44271005+krujeen@users.noreply.github.com>
2020-10-01 12:26:40 +0700

    Create add_list.py

---
+++ add_list.py
@@ -0,0 +1,20 @@
+
+n = 10
+
+# การเพิ่มข้อมูลใน list โดยใช้ while
+list1 = []
+i = 0
+
+while i < n:
+    x = int(input())
+    list1.append(x)
+    i=i+1
+print(list1)    
+    
+
+# การเพิ่มข้อมูลใน list โดยใช้ for range
+lists2 = []
+for i in range(0, 10, 1):
+    x = int(input())
+    lists2.append(x)
+print(lists2)
