edf31755f05aba01434a2b86acd3ea925bee1c45
KruJeen <44271005+krujeen@users.noreply.github.com>
2020-10-01 13:28:20 +0700

    Create list_info.py

---
+++ list_info.py
@@ -0,0 +1,11 @@
+# เก็บและแสดงข้อมูลใน list 
+list1 = ["apple", "banana", "cherry"]
+print(list1)
+
+
+# แสดงข้มูลตัวแรกของ list
+print(list1[0])
+
+
+# แสดงข้มูลตัวสุดท้ายของ list
+print(list1[-1])
