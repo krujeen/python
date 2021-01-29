66cee80061c5db4b424c7598197102f1c1a40085
KruJeen <44271005+krujeen@users.noreply.github.com>
2021-01-29 14:07:52 +0700

    Update set_add.py

--- set_add.py
+++ set_add.py
@@ -7,3 +7,18 @@ for i in range (n):
     x = int(input())
     setA.add(x)
 print(setA)
+
+
+
+
+# setA 
+setA = {0}
+setA.remove(0)
+# อ่านข้อมูลหลาย ๆ ตัวที่ผู้ใช้ป้อนเข้ามาในบรรทัดเดียวกัน โดยข้อมูลแต่ละตัวคั่นด้วยช่องว่าง  แปลงเป็น integer แล้วเก็บไว้ใน list 
+setA = [int(e) for e in input().split()] 
+
+for x in setA:
+  print(x)
+
+
+
